51c1cd0e6a3ca13f9dfe8bd5e6d897336fce7795
ddorstijn <dannydorstijn1997@gmail.com>
2022-05-11 22:35:58 +0200

    Fix header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,32 @@ module.exports = {
   darkMode: 'class',
   content: ['content/**/*.md', 'layouts/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          100: '#f3eff0',
+          200: '#dbced1',
+          300: '#c2aeb2',
+          400: '#aa8e93',
+          500: '#926d75',
+          600: '#71555b',
+          700: '#513d41',
+          800: '#513d41',
+          900: '#100c0d',
+        },
+        'secondary': {
+          100: '#f5f1ec',
+          200: '#e2d4c7',
+          300: '#cfb7a1',
+          400: '#bc9b7c',
+          500: '#a97e56',
+          600: '#836243',
+          700: '#5e4630',
+          800: '#382a1d',
+          900: '#130e0a',
+        }
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
