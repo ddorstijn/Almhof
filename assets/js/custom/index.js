83aca7b97599809ddc1de9dfe63057e544c74a70
ddorstijn <dannydorstijn1997@gmail.com>
2022-05-19 16:53:01 +0200

    Fixed first layout with SCSS

--- assets/js/custom/index.js
+++ assets/js/custom/index.js
@@ -1,4 +1,11 @@
 import Splide from '@splidejs/splide';
+import flatpickr from "flatpickr";
+
+Date.prototype.addDays = function (days) {
+  const date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
 
 new Splide('.splide', {
   gap: '2rem',
@@ -13,4 +20,28 @@ if (window.netlifyIdentity) {
       });
     }
   });
+}
+
+const today = new Date();
+const pickerArrival = flatpickr("#picker-arrival", {
+  altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "d-m-Y",
+  defaultDate: today,
+  minDate: today,
+  onValueUpdate(selectedDates) {
+    updateMinDate(selectedDates[0])
+  }
+});
+
+const pickerDeparture = flatpickr("#picker-departure", {
+  altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "d-m-Y",
+  defaultDate: today.addDays(2),
+  minDate: today,
+});
+
+function updateMinDate(date) {
+  pickerDeparture.config.minDate = date;
 }
