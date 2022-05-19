92ca11002e291ef0da3c12c611534dce7fe8cfc5
ddorstijn <dannydorstijn1997@gmail.com>
2022-05-19 23:24:35 +0200

    Removed netlify for speed

--- assets/js/custom/index.js
+++ assets/js/custom/index.js
@@ -12,16 +12,6 @@ Date.prototype.addDays = function (days) {
   return date;
 };
 
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", (user) => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
-
 new Splide('.splide', {
   gap: '2rem',
   keyboard: true,
