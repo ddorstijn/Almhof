12bec8ab5b4a03a4ed8cdfb81aa8288b6e0ba97f
ddorstijn <dannydorstijn1997@gmail.com>
2022-05-20 10:38:47 +0200

    Added a bit of responsitivity

--- assets/js/custom/index.js
+++ assets/js/custom/index.js
@@ -15,6 +15,25 @@ Date.prototype.addDays = function (days) {
 new Splide('.splide', {
   gap: '2rem',
   keyboard: true,
+  perPage: 1,
+  mediaQuery: 'min',
+  breakpoints: {
+		640: {
+			perPage: 2,
+		},
+    768: {
+      perPage: 3,
+    },
+    1024: {
+      perPage: 4,
+    },
+    1280: {
+      perPage: 5
+    },
+    1536: {
+      perPage: 6
+    }
+  }
 }).mount();
 
 const today = new Date();
